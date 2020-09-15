564e4e1616c68a83ba9ac702a465dc364f8a9486
Riccardo Pasini <riccardo.pasini1992@gmail.com>
2020-09-15 15:52:39 +0200

    c'è un errore

---
+++ index.php
@@ -0,0 +1,29 @@
+<!-- Creare una variabile con un paragrafo di testo. -->
+<!-- Visualizzare a schermo il paragrafo con la relativa
+lunghezza e sostituire la badword passata in GET con tre *. -->
+
+<?php
+ //variabile con paragrafo
+$text = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
+//visualizzo a schermo il par con la lunghezza
+$length = strlen($text);
+
+
+//sostituisco la badword con ***
+$badword = $GET_["name"];
+$textcensored = str_replace($badword,"***",$text);
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+    <body>
+        <p><?php echo $text;?></p>
+        <p><?php echo $length . " caratteri di lunghezza";?></p>
+        <p><?php echo $textcensored;?></p>
+    </body>
+</html>
